dd5e3471a4ca267ea6bf4c6f065ad861aa4a6e30
Aaron Danton <aarondanton04@hotmail.com>
2026-05-17 22:40:29 +0100

    feat: add logging to the ICM library

--- libraries/icm40609D/icm40609D/icm40609D.c
+++ libraries/icm40609D/icm40609D/icm40609D.c
@@ -5,14 +5,22 @@
 #include <hardware/spi.h>
 #include <string.h>
 
+#include "logger/logger.h"
+
 #include "pico/stdlib.h"
 
 #define COMBINE_UINT8_2(hi, lo) ((uint16_t)(hi) << 8) | (uint16_t)(lo)
 
-void icm_spi_select(const icm_inst* icm, bool value) { gpio_put(icm->config.cs, !value); }
+void icm_spi_select(const icm_inst* icm, bool value)
+{
+    LOG_VERBOSE("ICM", "%d", !value);
+    gpio_put(icm->config.cs, !value);
+}
 
 void icm_spi_write(const icm_inst* icm, uint8_t reg, uint8_t* data, size_t len)
 {
+    LOG_VERBOSE("ICM", "%d<-", reg);
+
     uint8_t tx[1 + len];
     uint8_t rx[1 + len];
 
@@ -26,6 +34,8 @@ void icm_spi_write(const icm_inst* icm, uint8_t reg, uint8_t* data, size_t len)
 
 void icm_spi_read(const icm_inst* icm, uint8_t reg, uint8_t* values, size_t len)
 {
+    LOG_VERBOSE("ICM", "%d->", reg);
+
     uint8_t tx[1 + len];
     uint8_t rx[1 + len];
 
@@ -41,6 +51,8 @@ void icm_spi_read(const icm_inst* icm, uint8_t reg, uint8_t* values, size_t len)
 
 bool icm_init(icm_inst* icm, icm_config config)
 {
+    LOG_DEBUG("ICM", "");
+
     if (icm == NULL)
         return false;
 
@@ -78,12 +90,16 @@ bool icm_init(icm_inst* icm, icm_config config)
 
 void icm_set_bank(const icm_inst* icm, uint8_t bank)
 {
+    LOG_DEBUG("ICM", "%d", bank);
     icm->write(icm, ICM_REG_BANK_SEL, &bank, 1);
 }
 
 void icm_pwr_mgmt(
     const icm_inst* icm, uint8_t accel_mode, uint8_t gyro_mode, bool idle, bool disable_temp)
 {
+    LOG_DEBUG("ICM", "Accel Mode: %d | Gyro Mode: %d | Idle: %d | Enable Temp: %d", accel_mode,
+        gyro_mode, idle, disable_temp);
+
     uint8_t value = (accel_mode & 0b11) | ((gyro_mode & 0b11) << 2) | ((uint8_t)(!idle) << 4)
         | ((uint8_t)disable_temp << 5);
 
@@ -94,6 +110,8 @@ void icm_pwr_mgmt(
 
 void icm_gyro_config(icm_inst* icm, uint8_t fsr, uint8_t odr)
 {
+    LOG_DEBUG("ICM", "FSR: %d | ODR: %d", fsr, odr);
+
     uint8_t value = (fsr << 5) | odr;
 
     icm_set_bank(icm, ICM_BANK_0);
@@ -129,6 +147,8 @@ void icm_gyro_config(icm_inst* icm, uint8_t fsr, uint8_t odr)
 
 void icm_accel_config(icm_inst* icm, uint8_t fsr, uint8_t odr)
 {
+    LOG_DEBUG("ICM", "FSR: %d | ODR: %d", fsr, odr);
+
     uint8_t value = (fsr << 5) | odr;
 
     icm_set_bank(icm, ICM_BANK_0);
@@ -152,6 +172,8 @@ void icm_accel_config(icm_inst* icm, uint8_t fsr, uint8_t odr)
 
 uint8_t icm_get_id(const icm_inst* icm)
 {
+    LOG_DEBUG("ICM", "");
+
     icm_set_bank(icm, ICM_BANK_0);
 
     uint8_t id;
@@ -159,20 +181,31 @@ uint8_t icm_get_id(const icm_inst* icm)
     return id;
 }
 
-float convert_raw_temperature(int16_t temperature) { return ((float)temperature / 132.48f) + 25; }
+float convert_raw_temperature(int16_t temperature)
+{
+    LOG_DEBUG("ICM", "%d", temperature);
+
+    return ((float)temperature / 132.48f) + 25;
+}
 
 float convert_raw_accel(const icm_inst* icm, int16_t accel)
 {
+    LOG_DEBUG("ICM", "%d", accel);
+
     return ((float)accel / icm->sensor_correction.accel_ssf) * 9.80665f;
 }
 
 float convert_raw_gyro(const icm_inst* icm, int16_t gyro)
 {
+    LOG_DEBUG("ICM", "%d", gyro);
+
     return (float)gyro / icm->sensor_correction.gyro_ssf;
 }
 
 void icm_read_data(icm_inst* icm)
 {
+    LOG_DEBUG("ICM", "");
+
     uint8_t values[14];
     icm->read(icm, ICM_REG_0_TEMP_DATA1, values, 14);
 
@@ -188,6 +221,8 @@ void icm_read_data(icm_inst* icm)
 
 float icm_read_temperature(const icm_inst* icm)
 {
+    LOG_DEBUG("ICM", "");
+
     uint8_t values[2];
     icm->read(icm, ICM_REG_0_TEMP_DATA1, values, 2);
 
