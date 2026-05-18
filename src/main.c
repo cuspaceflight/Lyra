01c8f151b790b42bade2edd03233dcc14491d79c
Aaron Danton <aarondanton04@hotmail.com>
2026-05-18 23:13:10 +0100

    feat: add pseudo tasks that are executed at a set frequency

--- src/main.c
+++ src/main.c
@@ -1,22 +1,45 @@
 #include "pico/stdlib.h"
+#include <pico/stdio.h>
+#include <pico/time.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "defines.h"
 
+#include "bmp388/bmp388.h"
+#include "icm40609D/icm40609D.h"
 #include "sx126X/sx126X.h"
+
+#include "logger/logger.h"
 #include "sx126X/sx126X_defines.h"
 
-lora_inst lora;
+#define DEBUG
 
-int main()
-{
-    stdio_init_all();
-
-    while (!stdio_usb_connected()) {
-        sleep_ms(100);
+#define CRITICAL_ERROR(MSG)                                                                        \
+    while (1) {                                                                                    \
+        LOG_ERROR("ERR", MSG);                                                                     \
+        sleep_ms(100);                                                                             \
     }
 
-    lora_config lora_config_defaults = {
+#define ARRAY_SIZE(ARR) sizeof(ARR) / sizeof(ARR[0])
+
+typedef struct task_t {
+    void (*setup)();
+    void (*run)();
+
+    uint64_t delay;
+    uint64_t previous;
+} task;
+
+lora_inst lora;
+bmp_inst  bmp;
+icm_inst  icm;
+
+void setupLora(void)
+{
+    LOG_INFO("MAIN", "");
+
+    lora_config config = {
         .spi      = LORA_SPI_PORT,
         .miso     = LORA_MISO,
         .mosi     = LORA_MOSI,
@@ -28,156 +51,103 @@ int main()
         .syncword = 0x3444,
     };
 
-    if (!lora_init(&lora, lora_config_defaults)) {
-        printf("Failed to initialize LoRa");
+    if (!lora_init(&lora, config)) {
+        CRITICAL_ERROR("Failed to init LoRa");
     }
 
-    lora_set_modulation_params(&lora, LORA_SF_10, LORA_BW_125, LORA_CR_4_5, false);
+    lora_set_tx_params(&lora, CONFIG_LORA_POWER, CONFIG_LORA_RAMP_TIME);
+    lora_set_modulation_params(&lora, CONFIG_LORA_SF, CONFIG_LORA_BW, CONFIG_LORA_CR, false);
     lora_set_dio_irq_params(&lora, LORA_IRQ_RX_DONE | LORA_IRQ_TIMEOUT, 0, 0, 0);
     lora_clear_irq_status(&lora, LORA_IRQ_ALL);
+}
 
-    lora_set_packet_params(&lora, 0x08, false, 0xFF, true, false);
+void setupBMP(void)
+{
+    LOG_INFO("MAIN", "");
+    bmp_config config = {
+        .i2c  = BMP_I2C_PORT,
+        .sda  = BMP_SDA,
+        .scl  = BMP_SCL,
+        .addr = BMP_ADDR,
+    };
 
-    lora_set_buffer_base_address(&lora, 0, 0);
+    if (!bmp_init(&bmp, config)) {
+        CRITICAL_ERROR("Failed to load BMP");
+    }
 
-    lora_rx(&lora, 0xFFFFFF);
+    bmp_osr_config(&bmp, CONFIG_BMP_OSR_P, CONFIG_BMP_OSR_T);
+    bmp_odr_config(&bmp, CONFIG_BMP_ODR);
+    bmp_iir_config(&bmp, CONFIG_BMP_IIR);
+    bmp_pwr_config(&bmp, true, true, BMP_MODE_NORMAL);
+}
 
-    uint32_t len = 255;
-    char     message[len];
+void setupICM(void)
+{
+    LOG_INFO("MAIN", "");
+    icm_config config = {
+        .spi  = ICM_SPI_PORT,
+        .miso = ICM_MISO,
+        .mosi = ICM_MOSI,
+        .sck  = ICM_SCK,
+        .cs   = ICM_CS,
+    };
 
-    while (true) {
-        if (lora_has_received_packet(&lora)) {
-            if (!lora_is_packet_valid(&lora)) {
-                printf("Invalid Packet\n");
-                continue;
-            }
+    if (!icm_init(&icm, config)) {
+        CRITICAL_ERROR("Failed to load ICM");
+    }
 
-            uint8_t read = lora_read_message(&lora, (uint8_t*)message, len);
-            printf("RECEIVED: %.*s\n", read, message);
+    icm_pwr_mgmt(&icm, CONFIG_ICM_ACCEL_MODE, CONFIG_ICM_GYRO_MODE, false, false);
+    icm_accel_config(&icm, CONFIG_ICM_ACCEL_FSR, CONFIG_ICM_ACCEL_ODR);
+    icm_gyro_config(&icm, CONFIG_ICM_GYRO_FSR, CONFIG_ICM_GYRO_ODR);
+}
 
-            lora_clear_irq_status(&lora, LORA_IRQ_ALL);
-        }
+void runLora(void) { LOG_INFO("MAIN", ""); }
+void runBMP(void) { LOG_INFO("MAIN", ""); }
+void runICM(void) { LOG_INFO("MAIN", ""); }
 
-        sleep_ms(500);
+task tasks[] = {
+    { .setup = setupLora, .run = runLora, .delay = 500, .previous = 0 },
+    { .setup = setupBMP,  .run = runBMP,  .delay = 250, .previous = 0 },
+    { .setup = setupICM,  .run = runICM,  .delay = 100, .previous = 0 },
+};
+
+void setup(void)
+{
+#ifdef DEBUG
+    stdio_init_all();
+
+    while (!stdio_init_all()) {
+        sleep_ms(100);
+    }
+
+    LOG_INFO("MAIN", "USB Connected");
+#endif
+
+    for (size_t i = 0; i < ARRAY_SIZE(tasks); i++) {
+        tasks[i].setup();
     }
 }
 
-// int main()
-// {
-//     stdio_init_all();
-//
-//     while (!stdio_usb_connected()) {
-//         sleep_ms(100);
-//     }
-//
-//     lora_config lora_config_defaults = {
-//         .spi      = LORA_SPI_PORT,
-//         .miso     = LORA_MISO,
-//         .mosi     = LORA_MOSI,
-//         .sck      = LORA_SCK,
-//         .cs       = LORA_CS,
-//         .rst      = LORA_RST,
-//         .busy     = LORA_BUSY,
-//         .freq     = 868E6,
-//         .syncword = 0x3444,
-//     };
-//
-//     if (!lora_init(&lora, lora_config_defaults)) {
-//         printf("Failed to initialize LoRa");
-//     }
-//     lora_set_tx_params(&lora, 14, LORA_RAMP_TIME_200U);
-//
-//     lora_set_modulation_params(&lora, LORA_SF_10, LORA_BW_125, LORA_CR_4_5, false);
-//     lora_set_dio_irq_params(&lora, LORA_IRQ_TX_DONE | LORA_IRQ_TIMEOUT, 0, 0, 0);
-//     lora_clear_irq_status(&lora, LORA_IRQ_ALL);
-//
-//     bool ready_to_send = true;
-//
-//     uint32_t len = 50;
-//     char     message[len];
-//
-//     uint32_t counter = 0;
-//     while (true) {
-//         if (ready_to_send) {
-//             snprintf(message, len, "Hello, World: %d", counter);
-//
-//             ready_to_send = false;
-//             printf("Sending: '%s'\n", message);
-//
-//             lora_write_tx_message(&lora, (uint8_t*)message, len);
-//             lora_set_packet_params(&lora, 0x08, false, len, true, false);
-//             lora_tx(&lora, 0);
-//             counter++;
-//         }
-//
-//         if ((lora_get_irq_status(&lora) & LORA_IRQ_TX_DONE) != 0) {
-//             ready_to_send = true;
-//             lora_clear_irq_status(&lora, LORA_IRQ_TX_DONE);
-//         }
-//
-//         sleep_ms(500);
-//     }
-// }
+void loop(void)
+{
+    absolute_time_t time       = get_absolute_time();
+    uint64_t        current_ms = to_ms_since_boot(time);
 
-// #include "bmp388/bmp388.h"
-// #include "icm40609D/icm40609D.h"
+    while (1) {
+        time       = get_absolute_time();
+        current_ms = to_ms_since_boot(time);
 
-// bmp_inst bmp;
-// icm_inst icm;
-//
-// int main()
-// {
-//     stdio_init_all();
-//
-//     while (!stdio_usb_connected()) {
-//         sleep_ms(100);
-//     }
-//
-//     icm_config icm_config_defaults = {
-//         .spi  = ICM_SPI_PORT,
-//         .miso = ICM_MISO,
-//         .mosi = ICM_MOSI,
-//         .sck  = ICM_SCK,
-//         .cs   = ICM_CS,
-//     };
-//
-//     if (!icm_init(&icm, icm_config_defaults)) {
-//         printf("Failed to load icm\n");
-//     }
-//
-//     icm_pwr_mgmt(&icm, ICM_PWR_LOW_NOISE, ICM_PWR_LOW_NOISE, false, false);
-//     icm_accel_config(&icm, ICM_ACCEL_FSR_32g, ICM_ACCEL_ODR_2_KHZ);
-//     icm_gyro_config(&icm, ICM_GYRO_FSR_500, ICM_GYRO_ODR_2_KHZ);
-//
-//     bmp_config bmp_config_defaults = {
-//         .i2c  = BMP_I2C_PORT,
-//         .sda  = BMP_SDA,
-//         .scl  = BMP_SCL,
-//         .addr = BMP_ADDR,
-//     };
-//
-//     if (!bmp_init(&bmp, bmp_config_defaults)) {
-//         printf("Failed to load bmp\n");
-//     }
-//     bmp_osr_config(&bmp, BMP_OVERSAMPLE_X16, BMP_OVERSAMPLE_X2);
-//     bmp_odr_config(&bmp, BMP_ODR_100);
-//     bmp_iir_config(&bmp, BMP_IIR_COEF_7);
-//     bmp_pwr_config(&bmp, false, true, BMP_MODE_NORMAL);
-//
-//     sleep_ms(1000);
-//
-//     while (true) {
-//         uint8_t id = icm_get_id(&icm);
-//         icm_read_data(&icm);
-//
-//         float temp     = bmp_read_temperature(&bmp);
-//         float pressure = bmp_read_pressure(&bmp, temp);
-//         float altitude = bmp_calc_altitude(pressure, BMP_SEA_LEVEL);
-//
-//         printf("%8.1f | %5.3f %5.3f | %+8.6f %+8.6f %+8.6f | %+8.6f %+8.6f %+8.6f\n", pressure,
-//             temp, icm.sensor_data.temperature, icm.sensor_data.accel[0],
-//             icm.sensor_data.accel[1], icm.sensor_data.accel[2], icm.sensor_data.gyro[0],
-//             icm.sensor_data.gyro[1], icm.sensor_data.gyro[2]);
-//         sleep_ms(50);
-//     }
-// }
+        for (size_t i = 0; i < ARRAY_SIZE(tasks); i++) {
+            if (current_ms - tasks[i].previous >= tasks[i].delay) {
+                tasks[i].previous = current_ms;
+                tasks[i].run();
+            }
+        }
+    }
+}
+
+int main()
+{
+    setup();
+    loop();
+}
